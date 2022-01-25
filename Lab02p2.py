e795906872a2a947ed52c22f8cbb3876d83fe1c2
jack23shields@gmail.com <jack23shields@gmail.com>
2022-01-25 15:18:24 +0000

    Lab2pt2

---
+++ Lab02p2.py
@@ -0,0 +1,8 @@
+Places = ["Spain", "Portugal", "Croatia", "America", "Irelnad"]
+print(Places[0])
+print(Places[1])
+print(Places[2])
+print(Places[3])
+print(Places[4])
+print(sorted(Places))
+print(Places)
