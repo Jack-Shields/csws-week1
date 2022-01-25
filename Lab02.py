92853959905c80396bef49cd5ff58268bfbe266c
jack23shields@gmail.com <jack23shields@gmail.com>
2022-01-25 15:18:02 +0000

    Lab2

---
+++ Lab02.py
@@ -0,0 +1,7 @@
+Names = ["Jack", "Cian", "Leah", "Elliot", "Cai", "Eben"]
+print("Hello how are you " + Names[0])
+print("Hello how are you " + Names[1])
+print("Hello how are you " + Names[2])
+print("Hello how are you " + Names[3])
+print("Hello how are you " + Names[4])
+print("Hello how are you " + Names[5])
